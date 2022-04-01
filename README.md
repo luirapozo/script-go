ce983d689acc3f9d6d7731de50c6e518fc305acc
luirapozo <luirapozo@gmail.com>
2022-04-01 16:13:53 -0500

    main program finished

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Done with the objective of getting familiar with the github.com/bitfield/script library for creating scrips in golang.
 
-The program will create a file that writes "Hello World!" as many times as it has been run before.
+The program creates a file that writes "Hello World!" as many times as it has been run before, and a file that keeps track of all the times it has been excecuted.
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -12,24 +11,31 @@ import (
 func main() {
 	//create a file that tracks the times the .exe has bee run
 	script.Exec("bash -c 'touch number'").Stdout()
-	script.Exec("bash -c 'echo 1 >> number'").Stdout()
 
 	data, err := script.File("number").String()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%v\n", data)
+	if data == "" {
+		script.Exec("bash -c 'echo 1 > number'").Stdout()
+		value, err := script.File("number").String()
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = value
+	}
 	data = strings.Split(data, "\n")[0]
 
 	num, err := strconv.Atoi(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println(num)
 	//all the Hello World!'s that have been written are recorded
 	for i := 0; i < num; i++ {
 		script.Exec("bash -c 'echo Hello World! >> hola'").Stdout()
 	}
 	num++
+	script.Exec("bash -c 'echo executed >> hola'").Stdout()
+	data = "bash -c 'echo " + strconv.Itoa(num) + " > number'"
+	script.Exec(data).Stdout()
 }
